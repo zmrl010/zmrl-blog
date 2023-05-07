84f5475fee26c43ec20edd9ccb7e4f1f2832156a
zmrl <zmriel@gmail.com>
2023-05-07 19:29:25 -0400

    feat: setup navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,19 @@
-<script>
+<script lang="ts">
 	import Header from './Header.svelte';
-	import './styles.css';
+	import TopAppBar, { AutoAdjust } from '@smui/top-app-bar';
+	import './styles.scss';
+
+	let topAppBar: TopAppBar;
 </script>
 
 <div class="app">
-	<Header />
+	<Header bind:topAppBar title="zmrl" />
 
 	<main>
-		<slot />
+		<AutoAdjust {topAppBar}>
+			<slot />
+		</AutoAdjust>
 	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-		</p>
-	</footer>
 </div>
 
 <style>
@@ -33,23 +32,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
